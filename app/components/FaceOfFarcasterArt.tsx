5608c625496fc2c758871ccd7edaf8ff6a46d40a
Joe Bae <lineanity@gmail.com>
2025-02-27 11:13:28 +0700

    minor fix

--- app/components/FaceOfFarcasterArt.tsx
+++ app/components/FaceOfFarcasterArt.tsx
@@ -1,7 +1,7 @@
 import useAnimationFrames from "@/hooks/useAnimationFrames";
 
 export default function FaceOfFarcasterArt() {
-  const { containerRef } = useAnimationFrames("/farcaster-logo");
+  const { containerRef } = useAnimationFrames("/farcaster-logo.png");
   return (
     <main className="relative flex justify-center items-center w-full min-h-screen text-white">
 
